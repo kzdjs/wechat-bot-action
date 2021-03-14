de1c5b5a38e2f568f1d5dd397e6ceee3c769eb81
陈远翔 <cyxhuan9@163.com>
2021-03-14 16:42:02 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -8,24 +8,24 @@ module.exports =
 const core = __nccwpck_require__(769)
 const axios = __nccwpck_require__(81)
 
-try {
-  const url = core.getInput('wechat_bot_url')
-  const type = core.getInput('type')
-  const content = core.getInput('content')
+async function run() {
+  try {
+    const url = core.getInput('wechat_bot_url')
+    const type = core.getInput('type')
+    const content = core.getInput('content')
 
-  axios
-    .post(url, { msgtype: type, content })
-    .then(res => {
-      console.log('res', res)
-      core.setOutput('res', res)
-    })
-    .catch(err => {
-      console.log('err', err)
-      core.setOutput('err', err)
-    })
-} catch (error) {
-  core.setFailed(error.message)
+    console.log('type', type)
+    console.log('content', content)
+
+    const res = await axios.post(url, { msgtype: type, content })
+    console.log('res', res)
+  } catch (error) {
+    console.log('error', error)
+    core.setFailed(error.message)
+  }
 }
+
+run()
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const core = require('@actions/core')
 const axios = require('axios')
 
-try {
-  const url = core.getInput('wechat_bot_url')
-  const type = core.getInput('type')
-  const content = core.getInput('content')
+async function run() {
+  try {
+    const url = core.getInput('wechat_bot_url')
+    const type = core.getInput('type')
+    const content = core.getInput('content')
 
-  axios
-    .post(url, { msgtype: type, content })
-    .then(res => {
-      console.log('res', res)
-      core.setOutput('res', res)
-    })
-    .catch(err => {
-      console.log('err', err)
-      core.setOutput('err', err)
-    })
-} catch (error) {
-  core.setFailed(error.message)
+    console.log('type', type)
+    console.log('content', content)
+
+    const res = await axios.post(url, { msgtype: type, content })
+    console.log('res', res)
+  } catch (error) {
+    console.log('error', error)
+    core.setFailed(error.message)
+  }
 }
+
+run()
